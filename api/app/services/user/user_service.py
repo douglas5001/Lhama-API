c6738acb43defd37098a9b3e34988c988d818725
douglas5001 <douglasportella66@gmail.com>
2026-03-13 21:01:19 -0300

    feature: adicionando .gitignore

--- api/app/services/user/user_service.py
+++ api/app/services/user/user_service.py
@@ -8,16 +8,12 @@ class UserService:
         self.repository = UserRepository(db)
 
     def create_user(self, user_dto: UserCreate):
-        # Check business rule (duplicate e-mail)
         existing_user = self.repository.get_by_email(user_dto.email)
         if existing_user:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, 
                 detail="Email already registered in the platform"
             )
-        
-        # In a real scenario, we would encrypt the password here
-        # user_dto.password = get_password_hash(user_dto.password)
         
         return self.repository.create(user_dto)
 
